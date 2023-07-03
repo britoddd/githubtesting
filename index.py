e1dabd415eb90d924af2cc0fe3de121f46c589d6
Brian936 <brianvalerie7@gmail.com>
2023-07-03 19:23:23 +0700

    Added coordinate

--- index.py
+++ index.py
@@ -9,3 +9,12 @@ elif n < 0:
     print("n is negative")
 else:
     print("n is zero")
+
+
+index = "Budi"
+print(index[0])
+
+name = ["Budi", "Siti", "Joko"]
+print(name[2])
+
+coordinate = (10.0,20.0)

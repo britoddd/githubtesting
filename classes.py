a3c1f666a941617edff1abaf24b7abf8d2e7a4b0
Brian936 <brianvalerie7@gmail.com>
2023-07-10 20:05:42 +0700

    10 July

--- classes.py
+++ classes.py
@@ -3,4 +3,21 @@ class Flight():
         self.capacity = capacity
         self.passengers = []
 
-        
+    def add_passengers(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(2)
+
+people = ["Harry", "Ron", "Hermione"]
+for person in people:
+    success = flight.add_passengers(person)
+    if success:
+        print(f"Added {person} to flight successfully")
+    else:
+        print(f"No available seats for {person}")
--- index.py
+++ index.py
@@ -80,3 +80,14 @@ from function
 for i in range(10):
     print(f"The square of {i} is {function.square(i)}")  # harus dikasih penjelasan nama filenya
 '''
+
+#
+
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
